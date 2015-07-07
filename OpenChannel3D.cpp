4783c49d23ce6181bb0c7d2eaab7854ae6e9839e
Stu Blair <sblair@usna.edu>
2015-07-07 18:02:21 +0200

    optimised copy direction and read only bc values

--- OpenChannel3D.cpp
+++ OpenChannel3D.cpp
@@ -158,7 +158,9 @@ void OpenChannel3D::D3Q15_process_slices(bool isEven, const int firstSlice,
   //Nz=lastSlice-firstSlice;
   const int numSpd=15;
   #pragma omp parallel for collapse(3)
-  #pragma acc parallel loop collapse(3) copy(fIn[0:nnodes*numSpd], fOut[0:nnodes*numSpd], inl[0:nnodes], onl[0:nnodes], snl[0:nnodes], u_bc[0:nnodes])
+  #pragma acc parallel loop collapse(3) \
+    copyin(fIn[0:nnodes*numSpd]) copyout(fOut[0:nnodes*numSpd]) \
+    present(inl[0:nnodes], onl[0:nnodes], snl[0:nnodes], u_bc[0:nnodes])
   for(int Z=firstSlice;Z<lastSlice;Z++){
     for(int Y=0;Y<Ny;Y++){
       for(int X=0;X<Nx;X++){
--- wmBrick3D.cpp
+++ wmBrick3D.cpp
@@ -26,34 +26,45 @@ int main(int argc, char * argv[])
   if(rank==0){
 	  cout<< "Problem initialized!" << endl;
   }
-  // write initial data
-  // (data processing script will be expecting it)
-  pp.write_data(MPI_COMM_WORLD,true);
 
-  double time_start, time_end, ex_time, LPU_sec, gNumLP;
-  time_start = MPI_Wtime();
-  for(int ts = 0; ts<pp.Num_ts;ts++){
-	  // say something comforting about the problem progress
-	  if((ts+1)%(pp.ts_rep_freq)==0){
-		  if(rank==0){
-			  cout << "Executing time step number " << ts+1 << endl;
-		  }
+  int* inl = pp.inl;
+  int* onl = pp.onl;
+  int* snl = pp.snl;
+  float* u_bc = pp.u_bc;
+  int nnodes = pp.nnodes;
 
-	  }
-	  pp.take_lbm_timestep(ts%2==0,MPI_COMM_WORLD); // weird function call sig.
+  #pragma acc  data \
+      copyin(inl[0:nnodes], onl[0:nnodes], snl[0:nnodes], u_bc[0:nnodes])
+  {
+    // write initial data
+    // (data processing script will be expecting it)
+    pp.write_data(MPI_COMM_WORLD,true);
 
-	   if((ts+1)%(pp.plot_freq)==0){
-	   	  // write data at requested intervals.
-	   	  pp.write_data(MPI_COMM_WORLD,ts%2==0);
-	   }
-  }
-  time_end = MPI_Wtime();
+    double time_start, time_end, ex_time, LPU_sec, gNumLP;
+    time_start = MPI_Wtime();
+    for(int ts = 0; ts<pp.Num_ts;ts++){
+	    // say something comforting about the problem progress
+	    if((ts+1)%(pp.ts_rep_freq)==0){
+		    if(rank==0){
+			    cout << "Executing time step number " << ts+1 << endl;
+		    }
 
-  if(rank==0){
-	  ex_time = time_end - time_start;
-	  gNumLP = pp.Nx*pp.Ny*pp.Nz;
-	  LPU_sec = ((double)gNumLP*(double)pp.Num_ts)/ex_time;
-	  cout << "Estimiated LPU/sec = " << LPU_sec << endl;
+	    }
+	    pp.take_lbm_timestep(ts%2==0,MPI_COMM_WORLD); // weird function call sig.
+
+	     if((ts+1)%(pp.plot_freq)==0){
+	     	  // write data at requested intervals.
+	     	  pp.write_data(MPI_COMM_WORLD,ts%2==0);
+	     }
+    }
+    time_end = MPI_Wtime();
+
+    if(rank==0){
+	    ex_time = time_end - time_start;
+	    gNumLP = pp.Nx*pp.Ny*pp.Nz;
+	    LPU_sec = ((double)gNumLP*(double)pp.Num_ts)/ex_time;
+	    cout << "Estimiated LPU/sec = " << LPU_sec << endl;
+    }
   }
   MPI_Finalize();
   return 0;
